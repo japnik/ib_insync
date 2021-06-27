aee4dbe8ff5588396a23bcb9498f306349f2f2d5
japnik <japnik1@gmail.com>
2021-06-26 20:49:35 -0400

    update
    update 6/26

---
+++ notebooks/scheduler_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+import datetime as dt
+
+
+# In[7]:
+
+
+df = pd.DataFrame({'a':[1,2,3]})
+
+
+# In[8]:
+
+
+df
+
+
+# In[11]:
+
+
+df.to_csv('C:\\Users\\navsa\\Documents\\Python_Projects\\ib_insync\\notebooks\\Data\\Raw\\random12.csv')
+
+
+# In[ ]:
+
+
+
+
